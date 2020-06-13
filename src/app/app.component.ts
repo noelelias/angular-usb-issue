a97641fc8f103f4720c8a0cbd863bdb53db2f683
Noel <schenknoel@gmail.com>
2020-06-13 17:13:58 +0200

    after init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'usb-issue';
+	title='usb-issue';
+	clickMe() {
+		navigator.usb.requestDevice({
+			filters: [{}]
+		});
+	}
 }
